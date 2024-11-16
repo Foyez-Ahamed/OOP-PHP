bf56120dfafeb35f8c70a5482ac7f0bc93ba8a59
Foyez-Ahamed <foyez1435@gmail.com>
2024-11-16 16:00:24 +0600

    how to define class and access class properties and method

--- index.php
+++ index.php
@@ -1,6 +1,31 @@
 <?php 
 
-echo "Hello world"
+// Define class //
+class man {
 
+
+    // properties //
+    public $name = 'Foyez Ahamed';
+    public $age = '30';
+
+    public $mother_name = "Arafater Nesa";
+
+    public $father_name = "Nurul Amin";
+
+
+    // method //
+    function mySelf(){
+        return "Hello this is Foyez Ahamed";
+    }
+}
+
+// access class //
+
+$foyez = new man();
+
+echo "My mother name is" ." ". $foyez->mother_name . "<br>";
+
+// access method that means function //
+echo "This is Function that is method access" . " ". $foyez->mySelf();
 
 ?>
