2c74117dba03a0846d16dc02012b518d240a3788
Foyez-Ahamed <foyez1435@gmail.com>
2024-11-16 16:43:42 +0600

    access function dynamic way

--- index.php
+++ index.php
@@ -15,26 +15,33 @@ class man {
 
 
     // method //
-    function mySelf(){
-        return "Hello this is Foyez Ahamed";
+    function mySelf($myName, $motherName){
+        $this->name = $myName;
+        $this->mother_name = $motherName;
+
+        $info = "My name is" . " ". $this->name . " " . "My mother name is" . " " . $this->mother_name;
+
+        return $info;
     }
 }
 
 // access class //
 
+// create object //
 $foyez = new man();
 
-$myname = $foyez->name = "Foyez Ahamed";
+$foyez->name = "Foyez Ahamed";
+$foyez->age = "26";
+$foyez->mother_name ="Arafater Nesa";
+$foyez->father_name ="Nurul Amin";
 
-echo $myname . "<br>";
+echo $foyez->mySelf("Foyez Ahamed", "Arafater Nesa.");
 
 
-
-
+// create another object //
 $konok = new man();
 
 $myGfName = $konok->name = "Suraiya Zaman Konok";
 
-echo $myGfName;
 
 ?>
